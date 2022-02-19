05c27f995cbbf921b596b27667a8b19ef6462466
Tomek <tomek@mediasave.pl>
2022-02-19 23:57:30 +0100

    Wykluczenie autokonfiguracji źródła danych

--- src/main/java/pl/tomek/budżet/BudżetAplikacja.java
+++ src/main/java/pl/tomek/budżet/BudżetAplikacja.java
@@ -2,8 +2,9 @@ package pl.tomek.budżet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class BudżetAplikacja {
 
 	public static void main(String[] args) {
