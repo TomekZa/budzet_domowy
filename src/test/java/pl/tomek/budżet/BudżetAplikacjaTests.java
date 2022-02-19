7b18d148c4e85b4affe6a56a8f86e17c3976b448
Tomek <tomek@mediasave.pl>
2022-02-19 23:54:02 +0100

    Inicjalizacja projektu i repozytorium.

---
+++ src/test/java/pl/tomek/budżet/BudżetAplikacjaTests.java
@@ -0,0 +1,13 @@
+package pl.tomek.budżet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudżetAplikacjaTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
